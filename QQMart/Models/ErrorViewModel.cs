d11ecb284e224e2b873739c3e334177ef8725bee
Nelsar <51225765+Nelsar@users.noreply.github.com>
2022-09-13 20:32:00 +0600

    Add project files.

---
+++ QQMart/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QQMart.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
